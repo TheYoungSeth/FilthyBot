67b0774d8fed94723f9904d335ccc80a21040854
TheYoungSeth <50074755+TheYoungSeth@users.noreply.github.com>
2019-04-28 18:17:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ client = Bot(command_prefix="ff")
 @client.event
 async def on_ready():
     print("loading...")
-    await client.change_presence(game=discord.Game(name='with the ass flute'))
+    games = "with the ass flute", "with the nose flute"
+    await client.change_presence(game=discord.Game(name=random.choice(games)))
     print("loaded!")
 
 @client.event
 async def on_message(message):
     if message.content == "ffstop":
-        await client.send_message(message.channel, ' Its time to STOP! Its time to stop, ok? NO MORE! This-This-This is NOT OK! This needs to stop, NOW! This is CANCER! This is so much cancer, that I can feel the tumors Growing on my BACK!')
+        stop = "Its time to STOP! Its time to stop, ok? NO MORE!", "This-This-This is NOT OK! This needs to stop, NOW! This is CANCER! This is so much cancer, that I can feel the tumors Growing on my BACK!"
+        await client.send_message(message.channel, random.choice(stop))
 
 client.run("NTcxNzM3NTc0Njc1MzE2NzQ3.XMSGgw.rdr8GNOsk51QPvOPm06V2bUR9k8")
